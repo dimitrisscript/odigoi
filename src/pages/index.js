f024af5808f3d331270205c5d38716982bc46913
malakes-odigoi <vagrant@vagrant.vm>
2019-03-27 20:46:19 +0000

    Trivial: keywords fix.

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Αρχική" keywords={`μαλάκες|οδηγοί||φανάρια|πεζοδρόμια`.split(`|`)} />
+    <SEO title="Αρχική" keywords={`μαλάκες|οδηγοί|φανάρια|πεζοδρόμια`.split(`|`)} />
     <Image />
   </Layout>
 )
