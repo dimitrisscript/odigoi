49a1d71fdbde9cb7f65b342795a948ad969320c8
malakes-odigoi <malakes-odigoi@gmail.com>
2019-03-27 14:39:58 +0000

    Structure and styling fixes.

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,10 +7,36 @@
 
 import React from "react"
 import PropTypes from "prop-types"
-import { StaticQuery, graphql } from "gatsby"
+import { StaticQuery, graphql, Link } from "gatsby"
 
-import Header from "./header"
 import "./layout.css"
+import "./layout-site.css"
+
+const Header = ({ siteTitle }) => (
+  <header className="Header">
+    <div>
+      <h1>
+        <Link to="/">{siteTitle}</Link>
+      </h1>
+    </div>
+  </header>
+)
+
+const Footer = () => (
+  <footer className="Footer">
+    <ul>
+      <li>
+        <Link to="/">Αρχική</Link>
+      </li>
+      <li>
+        <Link to="/about">Σχετικά</Link>
+      </li>
+      <li>
+        <a href="https://github.com/malakes-odigoi/website">Πιρούνιασε με στο GitHub</a>
+      </li>
+    </ul>
+  </footer>
+)
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -35,10 +61,7 @@ const Layout = ({ children }) => (
           }}
         >
           <main>{children}</main>
-          <footer style={{ borderTop: "1px solid gray", marginTop: "1em" }}>
-            <p>malakes-odigoi.com © {new Date().getFullYear()}</p>
-            <p><a href="https://github.com/malakes-odigoi/website" title="Fork me on GitHub">Fork me</a></p>
-          </footer>
+          <Footer />
         </div>
       </>
     )}
---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const AboutPage = () => (
+  <Layout>
+    <SEO title="Σχετικά" />
+    <p>TODO...</p>
+    <Link to="/">Πίσω στην αρχική!</Link>
+  </Layout>
+)
+
+export default AboutPage
