2db7bcb2bb3d3befed50f25436a191c9273d9806
malakes-odigoi <vagrant@vagrant.vm>
2019-03-27 05:20:22 +0000

    Test.

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Μαλάκες Οδηγοί!</h1>
+    <h1>Μαλάκες Οδηγοί!!!</h1>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
