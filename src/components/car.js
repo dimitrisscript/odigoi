5db1ac14127660919178c28af16c573789c09bf7
malakes-odigoi <malakes-odigoi@gmail.com>
2019-03-28 11:08:35 +0000

    Cars component and cars data.

--- src/components/car.js
+++ src/components/car.js
@@ -1,8 +1,26 @@
 import React from "react"
 import PropTypes from "prop-types"
 
-function Car({ image, title, description, tags }) {
-  return <p>car: {title}</p>
+function Car({ image, title, description, tags, date }) {
+  return (
+    <div className="Car">
+      <h3>{title}</h3>
+      <p>
+        <img src="https://placekitten.com/200/60" alt="todo" />
+      </p>
+      {description && <p className="description">{description}</p>}
+      <div className="meta">
+        {tags.length > 0 && (
+          <ul className="tags">
+            {tags.map(tag => (
+              <li key={tag}>{tag}</li>
+            ))}
+          </ul>
+        )}
+        <time dateTime={date}>{date}</time>
+      </div>
+    </div>
+  )
 }
 
 Car.propTypes = {
@@ -10,6 +28,7 @@ Car.propTypes = {
   title: PropTypes.string.isRequired,
   description: PropTypes.string,
   tags: PropTypes.array,
+  date: PropTypes.string.isRequired,
 }
 
 export default Car
---
+++ src/data.js
@@ -0,0 +1,15 @@
+export const cars = [
+  {
+    image: "icon.png",
+    title: "item 1",
+    description: "lorem ipsum!",
+    tags: "Παρκάρισμα|foo|bar".split("|"),
+    date: "2019-01-02",
+  },
+  {
+    image: "icon2.png",
+    title: "item 2",
+    tags: "Παρκάρισμα|foo|bar".split("|"),
+    date: "2019-01-31",
+  }
+]
